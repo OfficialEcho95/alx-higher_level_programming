1dfadc39080a5102a8a103a9676d3a3c9ede8175
Emmanuel Chukwu <officialecho95@outlook.com>
2022-11-29 20:22:00 +0000

    Initial

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/pyton
 def islower(c):
-    for letter in range(ord(97), ord(123)):
-        if islower(c) not in chr(letter):
-            return True
-        else:
-            return False
+    if islower(c) >= ord('a') and islower(c) <= ord('z'):
+        return True
+    else:
+        return False
