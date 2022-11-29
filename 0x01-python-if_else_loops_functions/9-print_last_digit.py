79ff0a9d155180a16a78cdc5c7c0a6673da69933
Emmanuel Chukwu <officialecho95@outlook.com>
2022-11-29 21:58:28 +0000

    Initial

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last = int(repr(abs(number))[-1])
+    last = int(repr(number)[-1])
     print("{}".format(last), end="")
+
+    return (abs(number) % 10)
