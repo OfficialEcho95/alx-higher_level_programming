470a6b2db4f57b12013b44d9852ce3d6938c90b6
Emmanuel Chukwu <officialecho95@outlook.com>
2022-11-29 22:09:26 +0000

    Initial

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for m in range(101):
+        if m % 3 == 0:
+            print("Fizz")
+        elif m % 5 == 0:
+            print("Buzz")
+        elif m % 3 == 0 and m % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("{}".format(m), end=" ")
