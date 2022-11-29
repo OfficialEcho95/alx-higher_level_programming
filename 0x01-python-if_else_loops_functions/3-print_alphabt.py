cf1ddef70a64e700583f61527befd96c3c1f9cc0
Emmanuel Chukwu <officialecho95@outlook.com>
2022-11-29 16:59:53 +0000

    Initial

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 # prints the ASCII alphabet, in lowercase, not followed by a new line
 
 for letter in range(97, 123):
-    if letter not in [101,113]:
+    if letter not in [101, 113]:
         print("{}".format(chr(letter)), end="")
