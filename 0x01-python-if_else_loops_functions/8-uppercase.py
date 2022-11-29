eba720aa0a596d7936ff6f5ddc039cf33f9deb4a
Emmanuel Chukwu <officialecho95@outlook.com>
2022-11-29 21:36:32 +0000

    Initial

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for m in str:
         if ord(str) >=97 and ord(str) <=122:
             str = chr(ord(m))
-            print("{}".format(chr(ord(m))))
+            print("{}".format(str))
