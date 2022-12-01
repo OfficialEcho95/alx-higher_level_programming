4ea9858e15194132f3f59d251a9714de8aba101b
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-01 14:57:25 +0000

    Initial

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import add_0
+
+a = 1
+b = 2
+
+print("{} + {} = {}".format(1, 2, (add_0.add(a, b))))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Here, in this module we looked at the usefulness and usages of imports and all that it entails.
