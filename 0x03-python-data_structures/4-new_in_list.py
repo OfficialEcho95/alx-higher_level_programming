b8d53dff8694d49d7c09245fb4a7fab7608cb326
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-04 10:43:02 +0000

    Initial

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     if idx < 0:
         return my_list
     elif idx > len(my_list):
@@ -8,4 +9,4 @@ def new_in_list(my_list, idx, element):
 
     else:
         my_list[idx] = element
-        return my_list
+        return new_list
