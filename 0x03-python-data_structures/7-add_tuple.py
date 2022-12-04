92c3f9da4fa0601a7413f3b2973eb363001c5a71
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-04 12:53:38 +0000

    Initial

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(map(lambda(i,j:i + j, tuple_a, tuple_a)))
+    # return tuple(map(lambda(i,j:i + j, tuple_a, tuple_a)))
+    new_tuple = tuple_a + tuple_a
+    return new_tuple
