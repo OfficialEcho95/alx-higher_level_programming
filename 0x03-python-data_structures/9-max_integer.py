4b3c2afeda24a42189b48398c9db558424925621
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-05 14:13:11 +0000

    Initial

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    myMax = my_list[0]
 
     if len(my_list) == 0:
         return "None"
 
     for num in my_list:
+        myMax = my_list[0]
         if myMax < num:
             myMax = num
     return (myMax)
