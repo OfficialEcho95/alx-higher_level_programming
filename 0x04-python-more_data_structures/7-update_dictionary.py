73df1d52fa49d03eb07a4fb7d7be7ede3788e6b4
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-09 07:02:30 +0000

    Initial

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def update_dictionary(a_dictionary, key, value):
     if key != a_dictionary:
         a_dictionary[key] = value
     for i in a_dictionary:
         if i == key:
              a_dictionary[key] = value
-    return a_dictionary
+    return (a_dictionary)
