d030fe6524553e35646e208d26193e49314da266
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-22 04:33:48 +0000

    Initial

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """THis function is the getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        """
+        Here this function is the setter
+        Args:
+            value(int): value to be resturned to the self.__size caller
+         """
+        self.__size = value
+        
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        
+    def my_print(self):
+        if self.__size != 0:
+            print()
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+        else:
+            print()
