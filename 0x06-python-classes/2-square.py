3b11d2e93aacde8760fad3a87b0fd116502126ad
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-22 03:30:00 +0000

    Initial

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,9 +13,8 @@ class Square:
             size (int): size of a side of the square
         Returns: None
         """
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
