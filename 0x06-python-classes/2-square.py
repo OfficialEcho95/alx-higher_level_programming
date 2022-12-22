9c24afa8165c6fcde0f0908da5bdfa9e1288b4d1
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-22 03:09:22 +0000

    Initial

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        try:
+            self.__size = size
+
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
