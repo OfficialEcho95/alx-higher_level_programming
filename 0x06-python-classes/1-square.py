62ed92a488278747a2ee0cc41bab26a028016feb
Emmanuel Chukwu <officialecho95@outlook.com>
2022-12-22 02:46:28 +0000

    Initial

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defining a class square with attribute """
+
+
+class Square:
+    """ declared class """
+    
+    def __init__(self, size):
+        """
+        __init__: my init method
+        Args:
+            size(int): calculating the value of the square
+        """
+        self.__size = size
+
