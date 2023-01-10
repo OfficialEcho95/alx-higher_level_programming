9b88837f2bb12e44ec80d7ef85a7fd1b0a3c6635
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 15:22:16 +0000

    Initial

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A rebel class"""
+class MyInt(int):
+    """this class inherits the int function but inverts it"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
