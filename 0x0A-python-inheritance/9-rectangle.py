a9aecddb48218dc79794b4da9df127fd8649dceb
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 15:00:08 +0000

    Initial

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+ Contains an empty class
+"""
+class BaseGeometry:
+    """attributes and properties will be added"""
+        
+    def area(self):
+        """ instance attribute """
+        raise Exception("area() is not implemented")
+        
+    def integer_validator(self, name, value):
+        """a function def integer_validator(self, name, value): that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ This class inherits the Basegeometry class"""
+    def __init__(self, width, height):
+        """instantiation of the method"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """returns the output of the class"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
