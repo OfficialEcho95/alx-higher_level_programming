e70a227add656849dac2ff97919262a54f354f4e
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 13:38:57 +0000

    Initial

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,4 +8,4 @@ class MyList(list):
     """This class inherits the builtin fuction list"""
     def print_sorted(self):
         """This contains the function that returns a sorted list"""
-        return print(sorted(sel
+        return print(sorted(self))
