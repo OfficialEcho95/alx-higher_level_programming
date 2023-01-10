42569bbde92f7136324982f1d09ca1b2d2ed33f7
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 13:49:03 +0000

    Initial

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """ This function returns true or false if the obj is is exactly an instance of the specified class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        False
