19a655a939013f2bd6d1ace9e89472275c237913
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 14:20:54 +0000

    Initial

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,10 +5,7 @@
 """
 class BaseGeometry:
     """attributes and properties will be added"""
-    def __init__(self, area=0):
-        """ Initializing area """
-        self.__area = area
-        
+
     def area(self):
         """ instance attribute """
         raise Exception("area() is not implemented")
