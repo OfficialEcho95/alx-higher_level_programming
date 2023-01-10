ec948e3c5c401cf268e628738acc9bc9e36fc2cf
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-10 13:38:10 +0000

    Initial

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This contains the MyList function
+"""
+
+
+class MyList(list):
+    """This class inherits the builtin fuction list"""
+    def print_sorted(self):
+        """This contains the function that returns a sorted list"""
+        return print(sorted(sel
