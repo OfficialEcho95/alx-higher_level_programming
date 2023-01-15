2b200e757d37d667e57854ccbaee4be9b3e2f3b7
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 21:42:46 +0000

    Initial

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,8 +7,6 @@ This module contains the function to_json_string(my_obj)
 
 
 def save_to_json_file(my_obj, filename):
-    """a function that writes an Object to a text file, using a JSON representation"""
-    json_object = json.dumps(my_obj)
-    
-    with open(filename) as f:
-        return f.read()
+    """a function that writes an Object to a text file, using a JSON representation"""    
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
