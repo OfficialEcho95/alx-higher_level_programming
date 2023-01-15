dd8469b888d3286c99afc98a97cca957b4ac9d61
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 21:13:10 +0000

    Initial

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,7 @@ This module contains the function to_json_string(my_obj)
 '''
 import json
 
+
 def to_json_string(my_obj):
     """A function that returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
