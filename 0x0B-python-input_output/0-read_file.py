9180633a718001387b16b71a672647ebff3cce01
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 20:08:18 +0000

    Initial

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     ''' This read_file function'''
-    with open("my_file_0.txt", "r", encoding = "utf-8") as f:
+    with open(filename) as f:
         print(f.read(), end="")
