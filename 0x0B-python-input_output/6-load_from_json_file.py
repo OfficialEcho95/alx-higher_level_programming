5f45fcafdcf0bd79471a665c02ce1e37818a6394
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 21:58:44 +0000

    Initial

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,6 +7,6 @@ import json
 
 
 def save_to_json_file(filename):
-    """a function that creates an object from JSON file"""
+    """A function that creates an object from JSON file"""
     with open(filename) as f:
         json.load(f)
