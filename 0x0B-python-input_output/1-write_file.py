62f25675f888eea4028faf3bf67e8494833da956
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 20:35:25 +0000

    Initial

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ This module contains the function
 def write_file(filename="", text=""):
     """ A  function that writes a string to a text file (UTF8) and returns the number of characters written """
     with open(filename, "w") as f:
-        f.write(text)
+        return f.write(text)
