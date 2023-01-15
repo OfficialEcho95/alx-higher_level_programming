8585940f77c54dcff4548b65091553e7ade80f20
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 21:54:30 +0000

    Initial

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+This module contains the function to_json_string(my_obj)
+'''
+import json
+
+
+def save_to_json_file(filename):
+    """a function that creates an object from JSON file"""
+    with open(filename) as f:
+        json.loads(f)
