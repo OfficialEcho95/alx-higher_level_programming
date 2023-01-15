47719757cf822dd91f134e85b4d56780edbba1da
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 20:09:02 +0000

    Initial

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-''' This function that reads a text file (UTF8) and prints it to stdout'''
+'''
+This function that reads a text file (UTF8) and prints it to stdout
+'''
+
+
 
 def read_file(filename=""):
     ''' This read_file function'''
