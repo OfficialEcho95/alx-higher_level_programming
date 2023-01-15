e02cf3bf518c1eca87856077268681cdae1c500a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-15 20:10:42 +0000

    Initial

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,7 @@ This function that reads a text file (UTF8) and prints it to stdout
 '''
 
 
-
 def read_file(filename=""):
-    ''' This read_file function'''
+    """ This read_file function"""
     with open(filename) as f:
         print(f.read(), end="")
