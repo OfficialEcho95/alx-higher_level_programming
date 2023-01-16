b16e3e7ca589f328b8bfbea47871ef65930c98cd
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-16 03:53:59 +0000

    Initial

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,13 +7,15 @@ the module that contains the function
 
 class Student:
     """A class of students"""
+
     def __init__(self, first_name, last_name, age):
         """Initialization of the instance variables"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
+        item = (list, dict, str, int, bool)
         obj_dict = {}
         if attrs is None:
             for key, value in self.__dict__.items():
@@ -21,6 +23,6 @@ class Student:
                     obj_dict[key] = value
         else:
             for key, value in self.__dict__.items():
-                if key in attrs and isinstance(value, (list, dict, str, int, bool)):
+                if key in attrs and isinstance(value, item):
                     obj_dict[key] = value
         return obj_dict
