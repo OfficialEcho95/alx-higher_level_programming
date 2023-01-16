14982febf337a6358144211a8598968984182f5e
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-16 03:29:19 +0000

    Initial

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,7 +12,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """returns the dict repesentation of the class"""
         return self.__dict__
