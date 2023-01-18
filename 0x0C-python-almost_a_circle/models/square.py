f7795eadc1aa82b8f7ef15f11f5d12b8da7c0842
Emmanuel Chukwu <officialecho95@outlook.com>
2023-01-18 05:44:19 +0000

    Initial

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,8 +3,8 @@
 This is the class module
 '''
 
-
-class Square:
+from models.rectangle import Rectangle
+class Square(Rectangle):
     """square class inheriring the rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
