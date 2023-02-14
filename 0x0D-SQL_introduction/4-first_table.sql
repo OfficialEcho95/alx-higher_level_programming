7319a447daf9f98daab5189f352506c0afe13ce8
Emmanuel Chukwu <Officialecho95@outlook.com>
2023-02-14 21:33:12 +0000

    created my first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- a script that creates a table called first_table
 -- in the current database in my MySQL server
-CREATE TABLE IF NOT EXISTS first_tab(id INT, name VARCHAR(256) not null);
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256) not null);
