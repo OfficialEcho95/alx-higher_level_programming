fa55c36bbd8b60769222c3d648e9482c37c9c832
Emmanuel Chukwu <Officialecho95@outlook.com>
2023-02-15 00:38:15 +0000

    prints all record of second table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score DESC, name;
+SELECT score, name WHERE name NOT NULL FROM second_table ORDER BY score DESC, name;
