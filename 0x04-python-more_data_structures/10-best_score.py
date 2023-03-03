b9b3ecb09db6e6c56159e707bd9fcac99f284252
Emmanuel Chukwu <Officialecho95@outlook.com>
2023-03-03 20:32:02 +0000

    best score of dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,11 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return "None"
 
-    print(max(a_dictionary))
+    return max(a_dictionary, key=a_dictionary.get)
+
+a_dictionary = {'Emma': 32, 'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
