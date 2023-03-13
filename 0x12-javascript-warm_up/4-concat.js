72b9d73e4206198bff7dc0b5054e24c3f46516e8
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 23:38:01 +0000

    concatenates 2 words is

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 if (process.argv[2]) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-	if (process.argv.length < 3) {
-		console.log('undefined is undefined');
-	}
+  if (process.argv.length < 3) {
+    console.log('undefined is undefined');
+  }
 }
