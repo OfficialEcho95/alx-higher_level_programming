4435c2514ce3250edcc958fe273c99bc6984706a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 16:39:22 +0000

    arguments to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2]) {
+	if (isNaN(+process.argv[2])) {
+		console.log('Not a number');
+	}
+	else {
+		console.log('My number:', process.argv[2]);
+	}
+} else {
+  console.log('No argument');
+}
