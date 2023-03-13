6acb3f1aaa3d83fea2b4508d4b996942126e0bcc
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 21:30:19 +0000

    max number of argument

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let a = process.argv.slice(2).map(arg => parseInt(arg));
+const a = process.argv.slice(2).map(arg => parseInt(arg));
 
-if (process.argv.length  < 3 || process.argv.length < 4) {
-	console.log(0);
+if (process.argv.length < 3 || process.argv.length < 4) {
+  console.log(0);
 } else {
-	console.log(Math.max(...a));
-	// console.log(Math.max(...a)) can be rewritten as
-	// console.log(Math.max.apply(null, args));
-
+  console.log(Math.max(...a));
+  // console.log(Math.max(...a)) can be rewritten as
+  // console.log(Math.max.apply(null, args));
 }
