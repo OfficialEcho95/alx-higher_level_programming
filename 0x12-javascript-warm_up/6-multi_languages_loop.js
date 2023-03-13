6cfc8e43de022cc1a58fcfa13fa2f7cb58749095
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 16:59:47 +0000

    loops without if/else

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,16 +2,16 @@
 let i = 0;
 
 while (i < 3) {
-	switch (i){
-		case 0:
-			console.log('C is fun');
-			break;
-		case 1:
-			console.log('Python is cool');
-			break;
-		case 2:
-			console.log('JavaScript is amazing');
-			break;
-			}
-	i++;
+  switch (i) {
+    case 0:
+      console.log('C is fun');
+      break;
+    case 1:
+      console.log('Python is cool');
+      break;
+    case 2:
+      console.log('JavaScript is amazing');
+      break;
+  }
+  i++;
 }
