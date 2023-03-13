bcbab56d8e76a89b17d09432b374d8a118a23315
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 15:55:25 +0000

    argument passed to it without length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-let numArgs = 0;
-
-for (let arg in args) {
-	numArgs++;
-}
-
-if (numArgs === 0) {
-  console.log('No argument');
-} else if (numArgs >= 1) {
-  console.log(args.join(' '));
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log("No argument");
 }
