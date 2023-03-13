81ea81e0be2a92e0cb2317833299704ebb693a8b
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 16:58:41 +0000

    loops without if/else

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let i = 0;
+
+while (i < 3) {
+	switch (i){
+		case 0:
+			console.log('C is fun');
+			break;
+		case 1:
+			console.log('Python is cool');
+			break;
+		case 2:
+			console.log('JavaScript is amazing');
+			break;
+			}
+	i++;
+}
