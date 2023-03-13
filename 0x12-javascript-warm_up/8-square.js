41fcac0dc4acd2f7240dc20fe596d7c546f11751
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 17:56:12 +0000

    square of the first argument

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let i = 0;
+
+if (process.argv[2]) {
+  if (isNaN(+process.argv[2])) {
+    console.log('Missing size');
+  } else {
+    for (; i < process.argv[2]; i++) {
+	let row = "";    
+	for (let j = 0; j < process.argv[2]; j++){
+		row += "x";
+	}
+	    console.log(row);
+    }
+  }
+}
