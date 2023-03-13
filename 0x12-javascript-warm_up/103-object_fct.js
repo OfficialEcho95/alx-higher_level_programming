f5554376d97dbccefd8b4fedb5513c3e895de63a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-13 23:12:10 +0000

    update script by adding function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,13 +2,12 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr() {
-	this.value++;
-	return this
-  }
 };
 console.log(myObject);
 
+myObject.incr = function () {
+	this.value++;
+};
 
 myObject.incr();
 console.log(myObject);
