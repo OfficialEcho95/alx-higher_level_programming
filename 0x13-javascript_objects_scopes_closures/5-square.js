321761c0b9a1a1bb3aef1a6c14ff09d2ccd0f3fc
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-15 13:27:26 +0000

    more features

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      let rec = '';
+      for (let col = 0; col < this.width; col++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+};
