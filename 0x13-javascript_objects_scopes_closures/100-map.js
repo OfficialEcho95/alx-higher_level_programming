85ae23d2d9394670f41e7f85225dc0066689a95d
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-15 22:49:49 +0000

    more features

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-
-const newList = list.map((value, index) => {
-  return value * index;
-});
-
-console.log('Initial list: ' + list);
-console.log('New list: ' + newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
