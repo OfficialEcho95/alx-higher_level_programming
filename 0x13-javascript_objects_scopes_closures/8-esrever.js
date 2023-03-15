b6e9155ec36e02ac7521c9462eb38fd2fe1e0946
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-15 20:02:53 +0000

    reverse given element

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esreve = function esrever (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
