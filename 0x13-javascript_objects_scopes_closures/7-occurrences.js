2ce368d9e3c9f9da4d6261657e65764af96850fd
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-15 19:50:33 +0000

    number of searched elements

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,10 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) { 
-	    // check if the current element matches the search element
-      count++; 
-	    // increment the count if a match is found
+    if (list[i] === searchElement) {
+      // check if the current element matches the search element
+      count++;
+      // increment the count if a match is found
     }
   }
   return count; // return the final count after checking all elements
