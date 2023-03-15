7bf6e7d6199f7ecf545cd166785baa64f4adc962
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-15 12:57:06 +0000

    more added functions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      let rec = '';
+      for (let col = 0; col < this.width; col++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
