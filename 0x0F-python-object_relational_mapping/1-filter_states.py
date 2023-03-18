bcee83acb445a1bd3c0fa9fd84a8a8506caeb5a3
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-18 08:03:51 +0000

    states starting from N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
                          db=db_name)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' 
-                 ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY id ASC")
 
     for row in cur.fetchall():
         print(row)
