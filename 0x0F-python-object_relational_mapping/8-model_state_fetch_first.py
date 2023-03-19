88ad0b762b9abbc33f4973303a9f4f305b949487
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-19 12:32:02 +0000

    sqlalchemy first states

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,9 +20,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).first()
-
-    if states is None:
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
