366d9f3dcef3cd5dbcfe5c2f41eb3e55113eb344
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-19 12:54:20 +0000

    sqlalchemy all states

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A module that prints the first state object from the database
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # we create a connection to the mysql server
+    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/\
+                           {db_name}')
+    Base.metadata.create_all(engine)
+
+    # session created
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
