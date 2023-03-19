72ab63275b5dae19b782b0185709674a4bf6bdfb
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-19 13:16:36 +0000

    sqlalchemy new state added

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create a connection to the MySQL server
+    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{db_name}')
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Add Louisiana to the database
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    # Print the new state's id
+    print(new_state.id)
+
+    # Close the session
+    session.close()
