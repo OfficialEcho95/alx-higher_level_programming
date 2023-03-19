e45cdf5f377d55fa334419d902316a4e7c698be3
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-19 13:33:45 +0000

    sqlalchemy updated state name

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,9 +8,6 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
 
     # create a connection to the mysql server
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
@@ -28,5 +25,3 @@ if __name__ == "__main__":
     state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
-
-    session.close()
