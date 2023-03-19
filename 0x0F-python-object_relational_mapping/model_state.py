023aff58b91a603b1df4742e39c5e578f5890a09
Emmanuel Chukwu <officialecho95@outlook.com>
2023-03-19 11:31:50 +0000

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,11 +14,5 @@ Base = declarative_base()
 class State (Base):
     """ the class inheriting the declarative base"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == '__main__':
-    from sqlalchemy import create_engine
-    engine = create_engine('mysql://localhost:3306/mydatabase')
-    Base.metadata.create_all(engine)
